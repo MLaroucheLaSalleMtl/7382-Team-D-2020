54030241709cfb83c0ff34f65b4ee7deebc70076
USBtheKey <38596161+USBtheKey@users.noreply.github.com>
2020-03-22 03:40:04 -0400

    Update

---
+++ 7382-Team-D-2020/Horror Escape Puzzle Game/Assets/Scripts/AudioControl.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+
+public class AudioControl : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioM;
+    [SerializeField] private Slider sVolMaster, sVolUI, sVolSFX, sVolMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        audioM.GetFloat("volMaster", out float fM);
+        audioM.GetFloat("volMusic", out float fMusic);
+        audioM.GetFloat("volUI", out float fUI);
+        audioM.GetFloat("volSFX", out float fSFX);
+
+        sVolMaster.value = fM;
+        sVolUI.value = fUI;
+        sVolSFX.value = fSFX;
+        sVolMusic.value = fMusic;
+
+    }
+
+    public void ControlMaster(float sliderValue)
+    {
+        audioM.SetFloat("volMaster", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void ControlSFX(float sliderValue)
+    {
+        audioM.SetFloat("volSFX", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void ControlMusic(float sliderValue)
+    {
+        audioM.SetFloat("volMusic", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void ControlUI(float sliderValue)
+    {
+        audioM.SetFloat("volUI", Mathf.Log10(sliderValue) * 20);
+    }
+
+}
---
+++ 7382-Team-D-2020/Horror Escape Puzzle Game/Assets/UpdateVolText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class UpdateVolText : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioM;
+
+    private Text txt;
+
+    private void Awake()
+    {
+        txt = GetComponent<Text>();
+    }
+
+    public void UpdateText(string paraName)
+    {
+        audioM.GetFloat(paraName, out float num);
+
+        num = (1 - (Mathf.Abs(num) / 80) )* 100;
+
+        txt.text = num.ToString() + "%";
+    }
+}
