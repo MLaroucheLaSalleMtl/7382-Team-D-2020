17d4bc586e35e918c8ae8e5ba4f50b2eff4854d9
USBtheKey <38596161+USBtheKey@users.noreply.github.com>
2020-04-07 00:14:29 -0400

    MASS ORGANIZATION - DEPRECATING ASSETS + SCRIPTS

---
+++ Horror Escape Puzzle Game/Assets/Scripts/Gameplay/LivingEntities/PlayerInputs.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class PlayerInputs : MonoBehaviour
+{
+
+    private MyInputs playerInputActions;
+    private bool locked = true;
+
+    [SerializeField] private float inputLockTimer = 0f; // TODO: Link to the something instead of gettign vlaue
+
+    public UnityEvent OnJumpCtx;
+
+    private void Awake()
+    {
+        playerInputActions = new MyInputs();
+        
+    }
+    private void OnEnable()
+    {
+        Invoke("Unlock", inputLockTimer);
+        playerInputActions.Enable();
+    }
+
+    private void Unlock() => locked = false; // Invoke Call
+
+    public void OnMoveCall(InputAction.CallbackContext context)
+    {
+        if(!locked) SetMovementVals(context.ReadValue<Vector2>());
+    }
+    private void SetMovementVals(Vector2 direction)
+    {
+        gameObject.GetComponent<Player_Behavior>().SetMovement(direction);
+    }
+
+    public void OnJumpCall(InputAction.CallbackContext context)
+    {
+        if (!locked) OnJumpCtx?.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        playerInputActions.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        playerInputActions.Dispose();
+    }
+}
---
+++ Horror Escape Puzzle Game/Assets/Scripts/Gameplay/LivingEntities/Player_Behavior.cs
@@ -0,0 +1,91 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class Player
+{
+
+    private static int life = 5; //default 5 lifes per session, num can go negative
+
+    public static int Life
+    {
+        get => life;
+        set => life = value > life ? life : value;
+    }
+}
+
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
+public class Player_Behavior: MonoBehaviour
+{
+    private UnityEvent OnDeath;
+
+    private Rigidbody2D rigid;
+
+    private Vector2 movement;
+
+    private SpriteRenderer sprt;
+
+    [SerializeField] private float speed; //default of 4
+    [SerializeField] private float upwardsVelocity; //default of 5
+
+    private bool canJump = true;
+
+    private void Awake()
+    {
+        OnDeath = new UnityEvent();
+
+        rigid = GetComponent<Rigidbody2D>();
+        sprt = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        if(GameManager.instance != null) OnDeath.AddListener(GameManager.instance.RespawnPlayer);
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.AddForce(transform.right * speed * movement.normalized);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.collider.CompareTag("Ground"))
+        {
+            Death();
+        }
+        else
+        {
+            canJump = true;
+            sprt.enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("DeathZone"))
+        {
+            Death();
+        }
+    }
+
+    public void Jump()
+    {
+        if (canJump)
+        {
+            rigid.AddForce(transform.up * upwardsVelocity, ForceMode2D.Impulse);
+            canJump = false;
+            sprt.enabled = false;
+        }
+    }
+
+    public void Death()
+    {
+        Player.Life--;
+        OnDeath?.Invoke();
+        Destroy(this.gameObject);
+    }
+
+    public void SetMovement(Vector2 direction) => movement = direction;
+
+}
